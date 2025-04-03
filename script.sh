342bd9022e7cf5ff0d61f7183a86574d3ba89878
PIENS <71657314+Herboe@users.noreply.github.com>
2025-04-03 18:13:16 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,10 +5,10 @@ URL="https://api.londonstockexchange.com/api/v1/components/refresh"
 DATA='{"path":"ftse-index","parameters":"indexname%3Dftse-100","components":[{"componentId":"block_content%3Aab7ff856-2875-48b9-a01a-33faefc45062","parameters":""},{"componentId":"block_content%3Adabc61a4-85a1-4ca1-816a-164bc7b79dd8","parameters":""},{"componentId":"block_content%3Ade3cfa38-4389-4729-93b7-6a35ac11ff4a","parameters":""},{"componentId":"block_content%3Aad7502b0-91e6-46a8-ac93-56877c3d2c44","parameters":""}]}'
 
 # Récupération de la réponse via curl
-JSON=$(curl -s "$URL" \
-  -H 'accept: application/json, text/plain, */*' \
+INFO=$(curl -s "$URL" \
+  -H 'accept: application/JSON, text/plain, */*' \
   -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7' \
-  -H 'content-type: application/json' \
+  -H 'content-type: application/JSON' \
   -H 'origin: https://www.londonstockexchange.com' \
   -H 'priority: u=1, i' \
   -H 'referer: https://www.londonstockexchange.com/' \
@@ -22,18 +22,18 @@ JSON=$(curl -s "$URL" \
   --data-raw "$DATA")
 
 # Extraction des différentes valeurs depuis le bloc "ftseticker"
-PRICE=$(echo "$JSON" | grep -oP '"name":"FTSE 100","description":"FTSE 100","value":\K[0-9]+\.[0-9]+' | head -n1 | tr -d '\n')
-PERCENT=$(echo "$JSON" | grep -oP '"name":"FTSE 100","description":"FTSE 100".*?"percentualchange":\K-?[0-9]+\.[0-9]+' | head -n1 | tr -d '\n')
-NETCHANGE=$(echo "$JSON" | grep -oP '"name":"FTSE 100","description":"FTSE 100".*?"netchange":\K-?[0-9]+\.[0-9]+' | head -n1 | tr -d '\n')
-HIGH=$(echo "$JSON" | grep -oP '"name":"FTSE 100","description":"FTSE 100".*?"high":\K[0-9]+\.[0-9]+' | head -n1 | tr -d '\n')
-LOW=$(echo "$JSON" | grep -oP '"name":"FTSE 100","description":"FTSE 100".*?"low":\K[0-9]+\.[0-9]+' | head -n1 | tr -d '\n')
+PRICE=$(echo "$INFO" | grep -oP '"name":"FTSE 100","description":"FTSE 100","value":\K[0-9]+\.[0-9]+' | head -n1 | tr -d '\n')
+PERCENT=$(echo "$INFO" | grep -oP '"name":"FTSE 100","description":"FTSE 100".*?"percentualchange":\K-?[0-9]+\.[0-9]+' | head -n1 | tr -d '\n')
+NETCHANGE=$(echo "$INFO" | grep -oP '"name":"FTSE 100","description":"FTSE 100".*?"netchange":\K-?[0-9]+\.[0-9]+' | head -n1 | tr -d '\n')
+HIGH=$(echo "$INFO" | grep -oP '"name":"FTSE 100","description":"FTSE 100".*?"high":\K[0-9]+\.[0-9]+' | head -n1 | tr -d '\n')
+LOW=$(echo "$INFO" | grep -oP '"name":"FTSE 100","description":"FTSE 100".*?"low":\K[0-9]+\.[0-9]+' | head -n1 | tr -d '\n')
 
 # Récupération de la date et l'heure actuelles
 TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
 
 CSVFILE="./data.csv"
 if [ ! -f "$CSVFILE" ]; then
-    echo "Timestamp,Price,PercentChange,NetChange,High,Low,LastClose,ClosingDate,PrevClosingDate,NetChangeSign,FiftyTwoWeeksMax,FiftyTwoWeeksMin,OneYearReturn" > "$CSVFILE"
+    echo "Timestamp,Price,PercentChange,NetChange,High,Low" > "$CSVFILE"
 fi
 
 # Sauvegarde des info
